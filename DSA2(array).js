6a772ab1aaee30bd47de5b7a799252925f813672
soumya <singhsoumyaa31@gmail.com>
2026-07-08 22:36:52 +0530

    add some advance Question

--- DSA2(array).js
+++ DSA2(array).js
@@ -45,4 +45,64 @@
 // }
 // console.log(arr)
 
+//To reduce the time complextity O n^2(Better By taking Extra Space)
+// (Left Rotation)
+// let arr=[1,2,3,4,5];
+// let temp=new Array(arr.length);
+// let k=2;
+// for(let i=0;i<arr.length;i++){
+//     temp[i]=arr[(i+k)%arr.length];
+// }
+
+// console.log(temp)
+
+//Right Rotation
+
+// let arr=[1,2,3,4,5];
+// let temp=new Array(arr.length);
+// let k=2;
+// for(let i=arr.length-1;i>=0;i--){
+//     temp[(i+k)%arr.length]=arr[i]
+// }
+// console.log(temp)
+
+//Best Appraoch
+//  (Left Rotation)
+
+// let arr=[1,2,3,4,5];
+// let k=2;
+// function reverse(i,j){
+//     while(i<j){
+//         let temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//         i++;
+//         j--;
+//     }
+// }
+// reverse(0,k-1);
+// reverse(k,arr.length-1);
+// reverse(0,arr.length-1);
+// console.log(arr);
+
+// Right Rotation
+
+// let arr=[1,2,3,4,5];
+// let k=2;
+// reverse(0,arr.length-1);
+// reverse(0,k-1);
+// reverse(k,arr.length-1);
+// function reverse(i,j){
+//     while(i<j){
+//         let temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//         i++;
+//         j--;
+//     }
+
+// }
+// console.log(arr);
+
+
 
