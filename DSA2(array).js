1c8625c406811b8c2be0dc31634d2309c1cbf32e
soumya <singhsoumyaa31@gmail.com>
2026-07-30 01:21:28 +0530

    add gas station

--- DSA2(array).js
+++ DSA2(array).js
@@ -311,3 +311,19 @@
 
 //     return ans;
 // };
+var canCompleteCircuit = function(gas, cost) {
+    let total=0;
+    let tank=0;
+    let start=0;
+    for(let i=0;i<gas.length;i++){
+        let diff=gas[i]-cost[i];
+        total+=diff;
+        tank+=diff;
+        if(tank<0){
+            start=i+1;
+            tank=0;
+        }
+    }
+    return total >= 0?start:-1;
+};
+console.log(canCompleteCircuit([1,2,3,4,5],[3,4,5,1,2]));
