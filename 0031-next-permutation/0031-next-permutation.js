5b723162eced5eb9693aa7e1a7e23a4c9f2dd301
Soumyaranjan Singh <158814057+Soumya7682@users.noreply.github.com>
2026-08-10 23:56:29 +0530

    Time: 1 ms (28.39%), Space: 57.8 MB (6.3%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -4,10 +4,12 @@
  */
 var nextPermutation = function(nums) {
     let n=nums.length;
+    //find 1st decreasing ele
     let i=n-2;
     while(i>=0&& nums[i]>= nums[i+1]){
         i--;
     }
+    //Find greater than nums[i]
     if(i>=0){
         let j=n-1;
         while(nums[j]<=nums[i]){
@@ -16,7 +18,7 @@ var nextPermutation = function(nums) {
         [nums[i],nums[j]]=[nums[j],nums[i]];
     }
 
-
+//Reverse remainpart
     let left=i+1;
     let right=n-1;
     while(left<right){
