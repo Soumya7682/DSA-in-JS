94674f4d4755572257fd4de50e3a15dc4bff3277
Soumyaranjan Singh <158814057+Soumya7682@users.noreply.github.com>
2026-08-12 23:28:02 +0530

    Time: 1 ms (98.99%), Space: 58.4 MB (48.82%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    let result=[]
+    let min=Math.min(...nums);
+    let max=Math.max(...nums);
+     let set=new Set(nums)
+     for(let i=min;i<=max;i++){
+        if(!set.has(i)){
+            result.push(i);
+        }
+     }
+     return result;
+};
