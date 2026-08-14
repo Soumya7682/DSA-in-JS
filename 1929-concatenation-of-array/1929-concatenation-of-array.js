fd784c292e86b53513c1ac4419ed06cfd542c97a
Soumyaranjan Singh <158814057+Soumya7682@users.noreply.github.com>
2026-08-14 23:15:13 +0530

    Time: 4 ms (6.41%), Space: 58.6 MB (39.02%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    // let arr;
+    // return arr=[...nums,...nums];
+    return [...nums,...nums]
+};
