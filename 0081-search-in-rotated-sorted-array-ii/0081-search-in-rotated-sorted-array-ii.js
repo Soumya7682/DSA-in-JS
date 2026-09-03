4dd9259acac0c7100bc186a0026433ff4d0388a4
Soumyaranjan Singh <158814057+Soumya7682@users.noreply.github.com>
2026-09-03 23:50:43 +0530

    Time: 0 ms (100%), Space: 53.4 MB (88.61%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    let l=0;
+    let h=nums.length-1;
+    while(l<=h){
+        let mid=Math.floor((l+h)/2);
+
+        if(nums[mid]===target){
+            return true;
+        }
+
+        if(nums[l]===nums[mid]&&nums[mid]===nums[h]){
+            l++;
+            h--;
+        }
+        else if(nums[l]<=nums[mid]){
+            if(nums[l]<=target && target < nums[mid]){
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        else{
+            if(nums[mid]<target && target <=nums[h]){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        
+    }
+    return false;
+};
